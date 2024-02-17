f8b8b995d67e2114e81d24826aac6722e7702a97
Kah Kit <kahkitzheng@gmail.com>
2024-02-18 00:28:38 +0100

    Fix: Initialise answer for singleplayer

--- src/routes/Singleplayer/SinglePlayerPage.tsx
+++ src/routes/Singleplayer/SinglePlayerPage.tsx
@@ -1,10 +1,11 @@
 import { useContext, useEffect, useMemo } from "react";
-import { randomId } from "../../utils";
+import { getNewWord, randomId } from "../../utils";
 import { GameContext } from "../../contexts/GameContext";
+import { words } from "../../constants/words";
 import Game from "../../components/Game";
 
 export default function SingleplayerPage() {
-  const { setUserId, setGameMode } = useContext(GameContext);
+  const { setUserId, setGameMode, setAnswer } = useContext(GameContext);
 
   const id = useMemo(() => randomId(), []);
 
@@ -18,6 +19,7 @@ export default function SingleplayerPage() {
   useEffect(() => {
     setUserId(id);
     setGameMode("single");
+    setAnswer(getNewWord(words));
   }, []);
 
   return <Game player={player} />;
